b3376f504632431e5d5e810f754e60da9d8bce64
KrukRecon <adam.kruk00@gmail.com>
2022-02-18 16:15:36 +0100

    Introduction to immutability by using records

---
+++ ClassesAndRecords/ClassesAndRecords/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassesAndRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pluralsightCourse = new Course();
+            pluralsightCourse.Name = "name";
+            pluralsightCourse.Author = "me";
+
+            var record = new CourseRecord("Records course", "me");
+        }
+    }
+
+    class Course
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+
+    public record CourseRecord(string Name, string Author);
+}
